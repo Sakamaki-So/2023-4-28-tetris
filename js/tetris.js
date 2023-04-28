6d1d92d04558b173ef3b0df48de9f9e2b8be7e2b
So Sakamaki <115204648+Sakamaki-So@users.noreply.github.com>
2023-04-28 15:39:02 +0900

    ミノの色を変更

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'cyan', 'orange', 'blue', 'yellow', 'red', 'green', 'purple'
+    'cyan', '#f4a460', 'blue', '#FFD052', 'red', 'lime', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
